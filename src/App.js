eb64767d1e3fd7a4258b89220ed4b95f1f8c92c4
Natasha <natasha.pinder00@gmail.com>
2021-08-31 14:44:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,12 +11,7 @@ function App() {
         
       </header>
      
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          ></a>
+      
     </div>
   );
 }
