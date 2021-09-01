9f94056caa63f8186632a5c06716300b032e8c81
Natasha <natasha.pinder00@gmail.com>
2021-09-01 10:26:28 +0100

    added a button to prove bootstrap is working

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo"
         alt="logo" />
+        <a hreg="#" className="btn btn-primary">Hiya</a>
         
       </header>
      
