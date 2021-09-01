12af3e143383496cc5ab97101fd6d947dc88e00d
Natasha <natasha.pinder00@gmail.com>
2021-09-01 12:04:18 +0100

    created a search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 
 export default function Dictionary() {
-    return (
-    <div className="Dictionary">
-<form>
-<input type="search" autoFocus={true}/>
+let [keyword, setKeyword] = useState("");
+
+function search(event){
+event.preventDefault() 
+alert(`Searching for ${keyword} definition`);
+}
+
+ function handleKeywordChange (event) {
+     setKeyword(event.target.value);
+ }
+
+return (
+<div className="Dictionary">
+<form onSubmit={search}>
+<input type="search" onChange={handleKeywordChange}/>
 </form>
-    </div>
-    );
+</div>
+);
 }
