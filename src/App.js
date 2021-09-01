7e89603269dd6f137eb841dabd8ed9cbb8f56db4
Natasha <natasha.pinder00@gmail.com>
2021-09-01 11:18:42 +0100

    created a form and dictionary js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import logo from "./logo.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo"
+        <img src={logo} className="App-logo img-fluid"
         alt="logo" />
-        
-      </header>
+        </header>
+        <main>
+          <Dictionary /> 
+          </main>
+          <footer
+          className="text-center">
+            Coded by Natasha Pinder
+          </footer>
      
       
+    </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Dictionary.css";
+
+
+export default function Dictionary() {
+    return (
+    <div className="Dictionary">
+<form>
+<input type="search" autoFocus={true}/>
+</form>
+    </div>
+    );
+}
