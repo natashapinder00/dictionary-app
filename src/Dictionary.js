2ef44d52653bafca5bdbd4cb635a1b9ccf9bc5a9
Natasha <natasha.pinder00@gmail.com>
2021-09-02 13:00:56 +0100

    added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,31 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 
 export default function Dictionary() {
 let [keyword, setKeyword] = useState("");
 
-function search(event){
+
+function handleResponse(response) { 
+console.log(response.data[0]);
+}
+
+function search(event) {
 event.preventDefault() 
 alert(`Searching for ${keyword} definition`);
 }
 
- function handleKeywordChange (event) {
+
+
+let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+axios.get(apiUrl).then(handleResponse);
+console.log(apiUrl);
+axios.get(apiUrl).then(handleResponse);
+
+
+
+function handleKeywordChange (event) {
      setKeyword(event.target.value);
  }
 
