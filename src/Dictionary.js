dcbdff89f637e6126aed9f845a6d8211f5ce4271
Natasha <natasha.pinder00@gmail.com>
2021-09-03 16:48:56 +0100

    added js string

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,45 +1,38 @@
 import React, { useState } from "react";
-import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
+import "./Dictionary.css";
 
 
 export default function Dictionary() {
 let [keyword, setKeyword] = useState("");
+let [results, setResults] = useState(null);
 
 
 function handleResponse(response) { 
-console.log(response.data[0]);
+setResults(response.data[0]);
 }
 
 function search(event) {
-event.preventDefault() 
-alert(`Searching for ${keyword} definition`);
-}
+event.preventDefault();
 
-
-// https://dictionaryapi.dev/
-
+// documentation - https://dictionaryapi.dev/e
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 axios.get(apiUrl).then(handleResponse);
-console.log(apiUrl);
-axios.get(apiUrl).then(handleResponse);
-
+}
 
 
 function handleKeywordChange (event) {
-     setKeyword(event.target.value);
+setKeyword(event.target.value);
  }
 
 return (
-<div className="Dictionary.App-logo {
-  max-width: 200px;
-  display: block;
-  margin: 0 auto;
-}
-">
+<div className="Dictionary">
 <form onSubmit={search}>
 <input type="search" onChange={handleKeywordChange}/>
 </form>
+
+<Results />
 </div>
 );
 }
