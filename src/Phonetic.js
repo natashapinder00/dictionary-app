1240be5e33ed803a829d14968a318a3a1e47a999
Natasha <natasha.pinder00@gmail.com>
2021-09-16 17:34:22 +0100

    exported the default function

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,15 +1,15 @@
+
 import React from "react";
 
 export default function Phonetic(props) {
-    
-        return ( 
-            <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank">
-                Listen 
-                </a>
-                <br />
-                {props.phonetic.text}
-                </div>
-        );
-            
+  console.log(props.phonetic);
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank">
+        Listen
+      </a>
+      <br />
+      {props.phonetic.text}
+    </div>
+  );
 }
