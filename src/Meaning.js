5e6c73a198893f3ed3cdc0dd5a6930c703051b21
Natasha <natasha.pinder00@gmail.com>
2021-09-16 14:43:35 +0100

    updated phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,13 +9,13 @@ export default function Meaning(props) {
 {props.meaning.definitions.map(function(definition, index) {
     return (
         <div key={index}>
-            <p>
+        
             <strong>Definition:</strong>{definition.definition}
             <br />
             <strong>Example:</strong>
             <em>{definition.example}</em>
             <Synonyms synomyns={definition.synomyns} />
-            </p>
+            
             </div>
     );
 })}
